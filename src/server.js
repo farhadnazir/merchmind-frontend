3f741753862077a098cf74bec878e693d78d4949
Farhad Nazir <farhadnazir1111@gmail.com>
2025-02-27 23:20:29 +0500

    Updated API URL to use Railway backend

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-export const backend_url = "http://localhost:8000/"
+export const backend_url = "https://merchmind-backend-production.up.railway.app"
 
 
-export const server = "http://localhost:8000/api/v2"
+export const server = "https://merchmind-backend-production.up.railway.app/api/v2"
